2475fe60c3af1eabd38ceb8928911c308f174835
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-05-07 00:55:35 +0530

    Time: 89 ms (31.94%), Space: 12.5 MB (98.92%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        n3 = len(nums) // 3
+        dic = {}
+        arr = []
+		
+        for num in nums:
+            if num not in dic:
+                dic[num] = 1
+            else:
+                dic[num] = dic[num] + 1
+				
+        for key in dic:
+            if dic[key] > n3:
+                arr.append(key)
+				
+        return arr
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
