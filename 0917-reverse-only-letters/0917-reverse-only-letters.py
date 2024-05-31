3fc69049897d9b5bcf699f56eedc75b765654020
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-05-31 19:27:43 +0530

    Time: 16 ms (44.09%), Space: 11.6 MB (88.5%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.py
+++ 0917-reverse-only-letters/0917-reverse-only-letters.py
@@ -1,21 +1,28 @@
 class Solution(object):
     def reverseOnlyLetters(self, s):
+        def is_alpha(c):
+            return ('a' <= c <= 'z') or ('A' <= c <= 'Z')
+        
         start = 0
-        end=len(s)-1
-        s=list(s)
-        while start<end:
-            if s[start].isalpha() and s[end].isalpha():
-                s[start],s[end]=s[end],s[start]
-                start +=1
+        end = len(s) - 1
+        s = list(s)
+        
+        while start < end:
+            if is_alpha(s[start]) and is_alpha(s[end]):
+                s[start], s[end] = s[end], s[start]
+                start += 1
                 end -= 1
-            elif s[start].isalpha() and not s[end].isalpha():
+            elif is_alpha(s[start]) and not is_alpha(s[end]):
                 end -= 1
-            elif not s[start].isalpha() and s[end].isalpha():
+            elif not is_alpha(s[start]) and is_alpha(s[end]):
                 start += 1
             else:
                 start += 1
                 end -= 1
+        
         return "".join(s)
+
+        
             
 
 
