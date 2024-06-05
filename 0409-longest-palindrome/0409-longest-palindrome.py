abd3c1d863e70cde738401d64cb6b1486963259f
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-06-05 23:35:31 +0530

    Time: 19 ms (46.07%), Space: 11.8 MB (30.01%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        hm = {}
+        for c in s:
+            if c in hm:
+                hm[c] += 1
+            else:
+                hm[c] = 1
+
+        ans = 0
+        isFirstOdd = False
+
+        for key in hm:
+            if hm[key] % 2 == 0:
+                ans += hm[key]
+            else:
+                ans += hm[key]
+                if not isFirstOdd:
+                    isFirstOdd = True
+                else:
+                    ans -= 1
+
+        return ans
+        """
+        :type s: str
+        :rtype: int
+        """
+        
