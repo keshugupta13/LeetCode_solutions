31033a1cb43ada4d9f8472ed351ddafcab4d0523
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-07-10 01:19:29 +0530

    Time: 2047 ms (27.21%), Space: 17.2 MB (5.49%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        unique_triplets = set()
+        result = []
+        for i in range(len(nums)):
+            left, right = i + 1 , len(nums) - 1
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    unique_triplets.add((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    left += 1
+        for triplet in unique_triplets:
+            result.append(list(triplet))
+        return result
