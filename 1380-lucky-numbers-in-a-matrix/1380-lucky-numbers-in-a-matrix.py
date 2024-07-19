a6ca0a91b606b5205df1d939ed9d2ed42928825f
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-07-19 23:53:55 +0530

    Time: 104 ms (21.26%), Space: 12 MB (22.05%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -1,29 +1,19 @@
 class Solution(object):
     def luckyNumbers (self, matrix):
-        N = len(matrix)
-        M = len(matrix[0])
+        row_mins = []
+        for row in matrix:
+            row_mins.append(min(row))
 
-        rowMin = []
-        for i in range(N):
-            rMin = float('inf')
-            for j in range(M):
-                rMin = min(rMin, matrix[i][j])
-            rowMin.append(rMin)
-
-        colMax = []
-        for i in range(M):
-            cMax = float('-inf')
-            for j in range(N):
-                cMax = max(cMax, matrix[j][i])
-            colMax.append(cMax)
-
-        luckyNumbers = []
-        for i in range(N):
-            for j in range(M):
-                if matrix[i][j] == rowMin[i] and matrix[i][j] == colMax[j]:
-                    luckyNumbers.append(matrix[i][j])
-
-        return luckyNumbers
+        rows = len(matrix)
+        cols = len(matrix[0])
+        luckies = []
+        for j in range(0,cols):
+            col = []
+            for i in range(0,rows):
+                col.append(matrix[i][j])
+            if max(col) in row_mins:
+                luckies.append(max(col))
+        return luckies
         """
         :type matrix: List[List[int]]
         :rtype: List[int]
