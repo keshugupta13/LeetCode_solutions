349bb1924f0e1a25deb22172b7a7b4f7cc9c4907
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-07-25 16:39:45 +0530

    Time: 1842 ms (17.23%), Space: 20.2 MB (72.02%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,19 +1,47 @@
-class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        def mergesort(A):
-            LA = len(A)
-            if LA == 1: return A
-            LH, RH = mergesort(A[:LA//2]), mergesort(A[LA//2:])
-            return merge(LH,RH)
+class Solution(object):
+    def mergeprocedure(self, nums, i, mid, j):
+        n1 = mid - i + 1
+        n2 = j - mid
 
-        def merge(LH, RH):
-            LLH, LRH = len(LH), len(RH)
-            S, i, j = [], 0, 0
-            while i < LLH and j < LRH:
-                if LH[i] <= RH[j]: i, _ = i + 1, S.append(LH[i])
-                else: j, _ = j + 1, S.append(RH[j])
-            return S + (RH[j:] if i == LLH else LH[i:])
-        
-        return mergesort(nums)	
-        
-        
+        Lsubarray = [0] * n1
+        Rsubarray = [0] * n2
+
+        for m in range(n1):
+            Lsubarray[m] = nums[i + m]
+        for n in range(n2):
+            Rsubarray[n] = nums[mid + 1 + n]
+
+        p = 0
+        q = 0
+        k = i
+        while p < n1 and q < n2:
+            if Lsubarray[p] <= Rsubarray[q]:
+                nums[k] = Lsubarray[p]
+                p += 1
+            else:
+                nums[k] = Rsubarray[q]
+                q += 1
+            k += 1
+
+        while p < n1:
+            nums[k] = Lsubarray[p]
+            p += 1
+            k += 1
+
+        while q < n2:
+            nums[k] = Rsubarray[q]
+            q += 1
+            k += 1
+
+    def mergesort(self, nums, i, j):
+        if i < j:
+            mid = i + (j - i) // 2
+            self.mergesort(nums, i, mid)
+            self.mergesort(nums, mid + 1, j)
+            self.mergeprocedure(nums, i, mid, j)
+        return nums
+
+    def sortArray(self, nums):
+        if len(nums) == 0:
+            return []
+        return self.mergesort(nums, 0, len(nums)-1)
