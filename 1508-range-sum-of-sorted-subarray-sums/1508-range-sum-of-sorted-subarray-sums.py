37fb954ff3779807fc60af0d083bf976e9c7a3bb
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-08-04 17:53:29 +0530

    Time: 321 ms (46.43%), Space: 31 MB (71.43%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.py
@@ -2,13 +2,13 @@ class Solution(object):
     def rangeSum(self, nums, n, left, right):
         MOD = 10 ** 9 + 7
         arr = []
-        i = 0
-        while i < n:
+        
+        for i in range(n):
             prefix = 0
             for j in range(i,n):
                 prefix += nums[j]
                 arr.append(prefix)
-            i += 1
+            
         arr.sort()
 
         sum1 = sum(arr[left-1:right])
