413df4e3bef0bd026465ba3f4a5b046abdb7eafd
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-08-11 18:13:10 +0530

    Time: 8 ms (94.18%), Space: 11.6 MB (79.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def isValid(self, s):
+        if len(s)%2!=0:
+            return 0
+            
+        if s[0]=="}" or s[0]== ")" or s[0]=="]":
+            return 0
+        
+        if s[len(s)-1] == "{" or s[len(s)-1] == "[" or s[len(s)-1] == "(":
+            return 0
+            
+        stack = []
+        for i in s:
+            if i=="{" or i=="(" or i=="[":
+                stack.append(i)
+                
+            else:
+                if len(stack) == 0:
+                    return 0
+                
+                if i=="]":
+                    if stack[len(stack)-1]=="[":
+                        stack.pop()
+                    else:
+                        return 0
+                        
+                elif i=="}":
+                    if stack[len(stack)-1]=="{":
+                        stack.pop()
+                    else:
+                        return 0
+                else:
+                    if stack[len(stack)-1]=="(":
+                        stack.pop()
+                    else:
+                        return 0
+        if len(stack)==0:
+            return 1
+        else:
+            return 0
