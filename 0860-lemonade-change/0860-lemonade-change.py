d6ef3b6fe0e7ecfaa6fcebcac1c8aa72a9952d62
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-08-16 20:29:50 +0530

    Time: 633 ms (66.73%), Space: 15.9 MB (53.64%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -3,23 +3,24 @@ class Solution(object):
         d = {}
         d[5] = 0
         d[10] = 0
-        d[20] = 0
-        for b in bills:
-            if b == 5:
-                d[b] += 1
-            elif b == 10:
+        d[15] = 0
+        for bill in bills:
+            if bill == 5:
+                d[bill] += 1
+            
+            elif bill == 10:
                 if d[5] == 0:
                     return False
                 d[10] += 1
                 d[5] -= 1
-            elif b == 20:
+
+            elif bill == 20:
                 if d[10] > 0 and d[5] > 0:
                     d[10] -= 1
-                    d[5] -= 1
-                elif d[5] >= 3:
-                    d[5] -= 3
+                    d[5]  -= 1
                 else:
-                    return False
-
+                    if d[5] >= 3:
+                        d[5] -= 3
+                    else:
+                        return False
         return True
-        
