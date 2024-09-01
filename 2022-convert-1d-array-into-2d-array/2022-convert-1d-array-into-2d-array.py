f0dbf5221ef7fd539fcd20f96d0937be8b5ddb8e
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-09-01 15:26:12 +0530

    Time: 771 ms (19.49%), Space: 20.2 MB (39.49%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.py
@@ -3,19 +3,8 @@ class Solution(object):
         if m * n != len(original):
             return []
         
-        res = []
-        index = 0
-        for i in range(m):
-            row = []
-            for j in range(n):
-                row.append(original[index])
-                index += 1
-            res.append(row)
-        return res
-        """
-        :type original: List[int]
-        :type m: int
-        :type n: int
-        :rtype: List[List[int]]
-        """
+        res = [[0] * n for _ in range(m)]  
+        for i in range(len(original)):
+            res[i // n][i % n] = original[i]  
         
+        return res
