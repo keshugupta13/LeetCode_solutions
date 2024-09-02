024cab1d402a8f13011e7514553582badba19f08
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-09-02 17:08:04 +0530

    Time: 25 ms (51.49%), Space: 11.6 MB (34.33%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -3,15 +3,16 @@ class Solution(object):
         n = len(colors)
         clr1 = colors[0]
         clr2 = colors[-1]
-        maxi = 0
+        maxi1 = 0
+        maxi2 = 0
         for i in range(n-1,-1,-1):
             if clr1 != colors[i]:
-                maxi = max(maxi,i)
+                maxi1 = max(maxi1,i)
                 break
         for i in range(n):
             if clr2 != colors[i]:
-                maxi = max(maxi,len(colors)-i-1)
+                maxi2 = max(maxi2,len(colors)-i-1)
                 break
-        return maxi
+        return max(maxi1,maxi2)
 
 
