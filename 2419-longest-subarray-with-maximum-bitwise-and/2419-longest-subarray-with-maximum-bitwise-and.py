8da33342b08469a9b1d307cee9cececcf750901d
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-09-14 23:52:41 +0530

    Time: 1429 ms (5.55%), Space: 23.3 MB (94.44%) - LeetHub

---
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestSubarray(self, nums):
+        ans, cnt = 0, 0
+        max_element = max(nums)
+        for num in nums:
+            if num == max_element:
+                cnt += 1
+            else:
+                cnt = 0
+            ans = max(ans, cnt)
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
