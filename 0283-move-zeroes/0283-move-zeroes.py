014e729a640d3ff9b89d1ddae672e5a21d1b311e
KESHU GUPTA13 <113796557+keshugupta13@users.noreply.github.com>
2024-09-20 21:57:30 +0530

    Time: 199 ms (24.22%), Space: 12.8 MB (62.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def moveZeroes(self, arr):
         n = len(arr)
-        j = -1
+        count = 0
         for i in range(n):
-            if arr[i] == 0:
-                j = i
-                break
-        
-        for i in range(j+1,n):
             if arr[i] != 0:
-                arr[j],arr[i] = arr[i],arr[j]
-                j += 1
+                arr[count] = arr[i]
+                count += 1
 
+        while count < n:
+            arr[count] = 0
+            count += 1
         return arr
-        
